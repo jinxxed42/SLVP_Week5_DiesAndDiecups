d43acd8a236870dd320ba09c3a16702369e05a17
Claus Damgaard <jinxxed@outlook.com>
2023-03-22 22:06:22 +0100

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLVP_Week5_DiesAndDiecups
+{
+    internal class Die
+    {
+        // Private fields naming according to https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
+        private static Random _rand;
+        private int _value;
+
+        public Die()
+        {
+            _rand = new Random();
+            _value = _rand.Next(1, 7);
+        }
+
+        public void Roll()
+        {
+            _value = _rand.Next(1, 7);
+        }
+
+        public int GetValue()
+        {
+            return _value;
+        }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using SLVP_Week5_DiesAndDiecups;
+
+Die d = new Die();
+Console.WriteLine("The value of the die is: " + d.GetValue());
+Console.WriteLine("Rolling the die...");
+d.Roll();
+Console.WriteLine("The value of the die is: " + d.GetValue());
